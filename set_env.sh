9ac27e668517ac933ba76016da4b95226a008909
Hoang Huynh <hoangpyy95@gmail.com>
2022-05-31 12:17:55 +0700

    Refactor to MS

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,21 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgres
+export POSTGRES_HOST=database-1.cygepmtcwnpw.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=hoang-269796998609-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
+
+set POSTGRES_USERNAME=postgres
+set POSTGRES_PASSWORD=postgres
+set POSTGRES_HOST=database-1.cygepmtcwnpw.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=postgres
+set AWS_BUCKET=hoang-269796998609-dev
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
